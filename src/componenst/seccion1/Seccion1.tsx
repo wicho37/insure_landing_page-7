fa587dc6259ae4cf0570083018a650902c3cc71c
wicho37 <willihernandezramirez@gmail.com>
2024-07-19 08:15:45 -0500

    debido a q le inserte el font family le cambie el css a vario componentes

--- src/componenst/seccion1/Seccion1.tsx
+++ src/componenst/seccion1/Seccion1.tsx
@@ -1,22 +1,21 @@
 import intro from "../../assets/img_mobil/intro-mobile.jpg"
 import style from "./Seccion1.module.css"
 import introRigth from "../../assets/img_mobil/bg-pattern-intro-right-mobile.svg"
+import introLeft from "../../assets/img_mobil/bg-pattern-intro-left-mobile.svg"
 
 
 const Seguro = () => {
      return(
-        <div>
-           <div>
-              <img src={intro} />
-           </div>
-           <div className={style.cobertura}>
-              <h2>Humanizing your insurance.</h2>
-              <p>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that's right for you. Ensure you and your loved ones are protected.</p>
-              <button className={style.boton2}>view plans</button>
-              <img src={introRigth} />
-           </div>
-
-        </div>
+         <div className={style.cobertura}>
+            <div>
+               <img src={intro} />
+            </div>
+            <img src={introLeft} alt="" className={style.introLeft1}/>
+            <h2>Humanizing your insurance.</h2>
+            <p>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that's right for you. Ensure you and your loved ones are protected.</p>
+            <button className={style.boton2}>view plans</button>
+            <img src={introRigth} className={style.introRigth1}/>
+         </div>
      )
 }
 
