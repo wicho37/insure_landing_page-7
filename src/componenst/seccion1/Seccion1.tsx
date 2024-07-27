0a2a0724b3aaf29eb5fe4d1a9de19662486700a5
wicho37 <willihernandezramirez@gmail.com>
2024-07-27 08:06:43 -0500

    se continua con el css para min-width

--- src/componenst/seccion1/Seccion1.tsx
+++ src/componenst/seccion1/Seccion1.tsx
@@ -10,11 +10,14 @@ const Seguro = () => {
             <div>
                <img src={intro} />
             </div>
+            <div>
             <img src={introLeft} alt="" className={style.introLeft1}/>
             <h2>Humanizing your insurance.</h2>
             <p>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that's right for you. Ensure you and your loved ones are protected.</p>
             <button className={style.boton2}>view plans</button>
             <img src={introRigth} className={style.introRigth1}/>
+
+            </div>
          </div>
      )
 }
