8c1822b776516d7bfc7938199bacdf18dd15c895
wicho37 <willihernandezramirez@gmail.com>
2024-07-29 08:15:19 -0500

    se continua con el css para la vista en desktop

--- src/componenst/seccion1/Seccion1.tsx
+++ src/componenst/seccion1/Seccion1.tsx
@@ -2,6 +2,8 @@ import intro from "../../assets/img_mobil/intro-mobile.jpg"
 import style from "./Seccion1.module.css"
 import introRigth from "../../assets/img_mobil/bg-pattern-intro-right-mobile.svg"
 import introLeft from "../../assets/img_mobil/bg-pattern-intro-left-mobile.svg"
+import introLeftDesk from "../../assets/img_desktop/bg-pattern-intro-left-desktop.svg"
+import introRigthDesk from "../../assets/img_desktop/bg-pattern-intro-right-desktop.svg"
 
 
 const Seguro = () => {
@@ -12,9 +14,11 @@ const Seguro = () => {
             </div>
             <div>
             <img src={introLeft} alt="" className={style.introLeft1}/>
+            <img src={introLeftDesk} alt="" className={style.introLeftDesk}/>
             <h2>Humanizing your insurance.</h2>
             <p>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that's right for you. Ensure you and your loved ones are protected.</p>
             <button className={style.boton2}>view plans</button>
+            <img src={introRigthDesk} alt="" className={style.introRigthDesk}/>
             <img src={introRigth} className={style.introRigth1}/>
 
             </div>
