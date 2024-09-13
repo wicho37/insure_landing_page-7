9b3b652811af78fc48482ef0a41a9d72b9f190b2
wicho37 <willihernandezramirez@gmail.com>
2024-09-13 07:53:16 -0500

    se agrega un componente para poder hacer el efecto overflow en la imagen introRight3 y q en el dom se vea lo mas parecido al del reto , se hace modificaciones en partes del css de la seccion1

---
+++ src/componenst/img_i_r_d/ImgIRD.tsx
@@ -0,0 +1,16 @@
+import introRight3 from "../../assets/img_desktop/bg-pattern-intro-right-desktop.svg"
+import style from "./ImgIRD.module.css"
+
+
+const ImgIRD = () => {
+
+    return (
+        <div className={style.content2}>
+            <img src={introRight3} className={style.introRight3} alt="" />
+        </div>
+    )
+}
+
+
+
+export default ImgIRD
--- src/componenst/seccion1/Seccion1.tsx
+++ src/componenst/seccion1/Seccion1.tsx
@@ -2,9 +2,10 @@ import intro from "../../assets/img_mobil/intro-mobile.jpg"
 import style from "./Seccion1.module.css"
 import introRigth from "../../assets/img_mobil/bg-pattern-intro-right-mobile.svg"
 import introLeft from "../../assets/img_mobil/bg-pattern-intro-left-mobile.svg"
-import introLeftDesk from "../../assets/img_desktop/bg-pattern-intro-left-desktop.svg"
 import introRigthDesk from "../../assets/img_desktop/bg-pattern-intro-right-desktop.svg"
+import introLeftDesk from "../../assets/img_desktop/bg-pattern-intro-left-desktop.svg"
 import {useState} from "react"
+import ImgIRD from "../img_i_r_d/ImgIRD"
 
 const Seguro = () => {
    const [formBoton, setFormBoton] = useState (false)
@@ -15,21 +16,24 @@ const Seguro = () => {
    }
      return(
          <div className={style.cobertura}>
-            <img src={introRigthDesk} alt="" className={style.introRigthDesktop}/>
-            <div>
-               <img src={intro} className={style.imgPpal}/>
-               
-            </div>
+             <ImgIRD />
             <div>
                <img src={introLeft} alt="" className={style.introLeft1}/>
+               <img src={introRigth} className={style.introRigth1}/>
+               <img src={intro} className={style.imgPpal}/> 
+            </div>
+
+            <div>
                <img src={introLeftDesk} alt="" className={style.introLeftDesktop}/> 
+            </div>
+            
+            <div className={style.contentText}>
                <span className={style.boton3}></span>
                <h2>Humanizing your insurance.</h2>
                <p>Get your life insurance coverage easier and faster. We blend our expertise and technology to help you find the plan that's right for you. Ensure you and your loved ones are protected.</p>
                <button className={style.boton2} onClick={pushBoton}>view plans</button>
-               <img src={introRigth} className={style.introRigth1}/>
-
             </div>
+            
          </div>
      )
 }
